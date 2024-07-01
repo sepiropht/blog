ece0d46c9196fa4aced37a82ff3c5e3926a61358
William Mbotta <sergembotta@mailoo.org>
2024-07-01 21:13:46 +0200

    revert origin

--- api/lnurlp.js
+++ api/lnurlp.js
@@ -1,31 +1,23 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+export default (_, res) => {
+  console.log('YESSSSSSS')
+  res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  res.setHeader('Access-Control-Allow-Methods', 'GET')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-
-  if (req.method === 'GET' && req.url === '/.well-known/lnurlp/sepiropht') {
-    res.status(200).json(
-      res.json({
-        status: 'OK',
-        tag: 'payRequest',
-        commentAllowed: 255,
-        callback: 'https://getalby.com/lnurlp/sepiropht/callback',
-        metadata:
-          '[["text/identifier","sepiropht@getalby.com"],["text/plain","Sats for Sepiropht"]]',
-        minSendable: 1000,
-        maxSendable: 11000000000,
-        payerData: {
-          name: { mandatory: false },
-          email: { mandatory: false },
-          pubkey: { mandatory: false },
-        },
-        nostrPubkey:
-          '79f00d3f5a19ec806189fcab03c1be4ff81d18ee4f653c88fac41fe03570f432',
-        allowsNostr: true,
-      })
-    )
-  } else {
-    res.status(404).json({ error: 'Not Found' })
-  }
+  res.json({
+    status: 'OK',
+    tag: 'payRequest',
+    commentAllowed: 255,
+    callback: 'https://getalby.com/lnurlp/sepiropht/callback',
+    metadata:
+      '[["text/identifier","sepiropht@getalby.com"],["text/plain","Sats for Sepiropht"]]',
+    minSendable: 1000,
+    maxSendable: 11000000000,
+    payerData: {
+      name: { mandatory: false },
+      email: { mandatory: false },
+      pubkey: { mandatory: false },
+    },
+    nostrPubkey:
+      '79f00d3f5a19ec806189fcab03c1be4ff81d18ee4f653c88fac41fe03570f432',
+    allowsNostr: true,
+  })
 }
